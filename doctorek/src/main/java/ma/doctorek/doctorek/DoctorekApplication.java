d663445da7c9637f5bfaaac8aa0dbcb59249cf0f
Akram BENHAMMOU <aben200244@gmail.com>
2026-03-31 12:29:49 +0100

    initial commit : US-01 Setup Docker + Spring Boot

---
+++ doctorek/src/main/java/ma/doctorek/doctorek/DoctorekApplication.java
@@ -0,0 +1,13 @@
+package ma.doctorek.doctorek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorekApplication.class, args);
+	}
+
+}
---
+++ doctorek/src/test/java/ma/doctorek/doctorek/DoctorekApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.doctorek.doctorek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
